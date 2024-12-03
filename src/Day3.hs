c332cc37923d576a1962ffa1341ecf6ebc84b3a1
Alastair White <airxhi@gmail.com>
2024-12-03 22:48:19 +0000

    chore: format

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,17 +1,15 @@
 module Day3 (day3) where
 
-
-import Data.List.Split ( splitOn )
-import Data.List (sort, group)
 import qualified Data.Bifunctor
+import Data.List (group, sort)
+import Data.List.Split (splitOn)
 import qualified Data.Map as Map
 import Text.Regex.TDFA ((=~))
 
-
 day3 :: String -> IO ()
 day3 inp = do
-    print $ part1 inp
-    print $ part2 inp
+  print $ part1 inp
+  print $ part2 inp
 
 data Op = Mul Int Int | Do | Don't
 
@@ -25,19 +23,19 @@ calculate :: (String -> [[String]]) -> String -> Int
 calculate f s = snd . foldl operate (True, 0) $ extractOperations f s
 
 operate :: (Bool, Int) -> Op -> (Bool, Int)
-operate (enabled, acc) op  = case op of
-    Do ->  (True, acc)
-    Don't -> (False, acc)
-    Mul a b -> (if enabled then (enabled, acc + a * b) else (enabled, acc))
+operate (enabled, acc) op = case op of
+  Do -> (True, acc)
+  Don't -> (False, acc)
+  Mul a b -> (if enabled then (enabled, acc + a * b) else (enabled, acc))
 
 extractOperations :: (String -> [[String]]) -> String -> [Op]
 extractOperations f xs = map findOp $ f xs
 
 findOp :: [String] -> Op
 findOp [_, m, a, b, d, _]
-    | m /= "" = Mul (read a) (read b)
-    | d /= "" = Do
-    | otherwise = Don't
+  | m /= "" = Mul (read a) (read b)
+  | d /= "" = Do
+  | otherwise = Don't
 
 r1 :: String -> [[String]]
 r1 xs = xs =~ "(mul)\\(([0-9]+),([0-9]+)\\)()()"
