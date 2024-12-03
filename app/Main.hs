3d12cd1f331c3f0a6102e7269c21d20996ab6670
Alastair White <airxhi@gmail.com>
2024-12-03 22:47:26 +0000

    feat: Day3

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import Day1
 import Day2
+import Day3
 
 run :: (String -> IO()) -> String -> IO ()
 run f fileName =
@@ -13,4 +14,5 @@ main :: IO ()
 main = 
     do
         -- run day1 "inputs/1.txt"
-        run day2 "inputs/2.txt"
+        -- run day2 "inputs/2.txt"
+        run day3 "inputs/3.txt"
---
+++ src/Day3.hs
@@ -0,0 +1,46 @@
+module Day3 (day3) where
+
+
+import Data.List.Split ( splitOn )
+import Data.List (sort, group)
+import qualified Data.Bifunctor
+import qualified Data.Map as Map
+import Text.Regex.TDFA ((=~))
+
+
+day3 :: String -> IO ()
+day3 inp = do
+    print $ part1 inp
+    print $ part2 inp
+
+data Op = Mul Int Int | Do | Don't
+
+part1 :: String -> Int
+part1 = calculate r1
+
+part2 :: String -> Int
+part2 = calculate r2
+
+calculate :: (String -> [[String]]) -> String -> Int
+calculate f s = snd . foldl operate (True, 0) $ extractOperations f s
+
+operate :: (Bool, Int) -> Op -> (Bool, Int)
+operate (enabled, acc) op  = case op of
+    Do ->  (True, acc)
+    Don't -> (False, acc)
+    Mul a b -> (if enabled then (enabled, acc + a * b) else (enabled, acc))
+
+extractOperations :: (String -> [[String]]) -> String -> [Op]
+extractOperations f xs = map findOp $ f xs
+
+findOp :: [String] -> Op
+findOp [_, m, a, b, d, _]
+    | m /= "" = Mul (read a) (read b)
+    | d /= "" = Do
+    | otherwise = Don't
+
+r1 :: String -> [[String]]
+r1 xs = xs =~ "(mul)\\(([0-9]+),([0-9]+)\\)()()"
+
+r2 :: String -> [[String]]
+r2 xs = xs =~ "(mul)\\(([0-9]+),([0-9]+)\\)|(do\\(\\))|(don't\\(\\))"
